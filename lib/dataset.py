069149b031a36b073b1f434871eda66d0f0bcc2f
LeeJiPyeong <77089771+jipyeong-lee@users.noreply.github.com>
2023-02-10 12:53:32 +0900

    Update dataset.py

--- lib/dataset.py
+++ lib/dataset.py
@@ -15,6 +15,10 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torchaudio import sox_effects, transforms
 
+from librosa import display
+import librosa.display
+
+
 
 class DefaultSet(Dataset):
     def __init__(self, root, subset, input_len, n_fft, sample_rate=None):
@@ -58,6 +62,13 @@ class DefaultSet(Dataset):
     def __len__(self) -> int:
         return len(self.files)
 
+    def remove_silence(self,audio,under=40):
+        audio = torch.squeeze(audio).numpy()
+        s = librosa.stft(audio, n_fft=800, hop_length=800//2)
+        db = librosa.amplitude_to_db(np.abs(s),ref = np.max)
+        remove_under_db = librosa.effects.split(audio,top_db = np.max(np.abs(db)) - under)
+        audio = torch.from_numpy(np.concatenate([audio[i[0]:i[1]] for i in remove_under_db]))
+        return torch.unsqueeze(audio,dim=0)
 
 class ContrastiveSet(DefaultSet):
     def __init__(self, root, subset, input_len, n_fft, pitch, stretch, sample_rate=None):
@@ -82,8 +93,22 @@ class ContrastiveSet(DefaultSet):
 
         return self.reshape(target, self.input_len)
 
+
+    def freq_masking(self, spectrogram, freq_masking_max_percentage=0.15):
+        spec = spectrogram.numpy()
+        _,all_freqs_num,_ = spec.shape 
+        freq_percentage = random.uniform(0.0, freq_masking_max_percentage)# 얼마만큼의 percentage로 마스킹을 할것인가
+              
+        num_freqs_to_mask = int(freq_percentage * all_freqs_num)
+        f3 = np.random.uniform(low=224.0, high=all_freqs_num - num_freqs_to_mask) # low를 f3가 시작하는 부분으로 잡아야함, low 수정
+        f3 = int(f3)
+        spec[0,f3:f3 + num_freqs_to_mask,:] = 0
+        return torch.from_numpy(spec)
+    
+
     def __getitem__(self, index):
         audio = self.load(index)
+        audio = self.remove_silence(audio)
         audio_orig = self.reshape(audio, self.input_len)
 
         reshape_func = functools.partial(self.reshape, length=self.input_len)
@@ -111,5 +136,10 @@ class ContrastiveSet(DefaultSet):
         audio_orig = self.fft(audio_orig)
         audio_pos = self.fft(audio_pos)
         audio_neg = self.fft(audio_neg)
+
+        #audio_orig = freq_masking(self.fft(audio_orig))
+        #audio_pos = freq_masking(self.fft(audio_pos))
+        #audio_neg = freq_masking(self.fft(audio_neg))
+
 
         return index, audio_orig, audio_pos, audio_neg
